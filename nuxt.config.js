e0ce268d450d892599e6c45c269501ba9ce1c250
ddong8 <donghaixing8@gmail.com>
2020-08-28 16:19:25 +0800

    change server ip & port.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,12 @@
 
 export default {
+  /*
+  ** Server config
+  */
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   mode: 'universal',
   /*
   ** Headers of the page
